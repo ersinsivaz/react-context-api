56c492ae20e1fa10f5442c51b5540d48af4cd651
ersin.sivaz@sbu.edu.tr <ersinsivaz@hotmail.com>
2019-10-25 16:34:03 +0300

    41 de kaldık

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react'
-
 import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
-
 import './App.css'
 import Navbar from './components/Navbar'
 import Users from './components/Users'
 import AddUser from './components/AddUser';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="container">
+      <Router>
+        <div className="container">
         <Navbar title="User App"></Navbar>
-        <hr></hr>
-        <AddUser />
-        <Users></Users>
-      
-      </div>
+          <hr></hr>
+          <Route exact path="/" component = {Users} />
+          <Route exact path="/add" component = {AddUser} />
+        </div>
+      </Router>
     )
   }
 }
 
 export default App;
+
+
